8f96dc87b04e494cba4e0a989d6db90dcaa7167e
Shawn1990 <oscar_va90@hotmail.com>
2011-04-30 15:33:20 +0100

    fix Buscador

--- application/views/header.php
+++ application/views/header.php
@@ -81,7 +81,7 @@
 		</div>
 			
 		<div>
-		<form class="buscador" action="<?php echo base_url();?>buscador/index" >
+		<form class="buscador" accept-charset="utf-8" method="post" action="<?php echo base_url();?>buscador/index">
 		<br/>
 		<input type='text' name='busqueda' id='busqueda' maxlength='30' size='20'/>
 		<input type='submit' value='Buscar'/>
